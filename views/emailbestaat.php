a9ac16a9fba9f60fcd3c93446d584b27cba72405
Larissa Marien <34274639+larissamarienAP@users.noreply.github.com>
2020-06-09 13:48:15 +0200

    Add new views
    Pagina's voor email bestaat al en fout login.

---
+++ views/emailbestaat.php
@@ -0,0 +1,18 @@
+<?php
+return "
+<div class='inlogkader'>
+		<img src='assets/Miauwer-Logo.png' alt='Logo miauwer' class='logo'>
+		<h1>Ai er liep iets fout!</h1>
+		<p>Met dit emailadres werd al een account gemaakt, probeer hiermee in te loggen.</p>
+
+		<a class='teruginloggen' href='index.php?pagina=login'>inloggen</a>
+
+
+
+		<div class='registreerkader'>
+			<p class='registerlink'>Nog geen miauwer-account?</p>
+			<p class='registerlink'> Maak er dan <a href='index.php?pagina=registratie'>hier</a> een aan!</p>
+		</div>
+</div>
+";
+?>
---
+++ views/foutwachtwoord.php
@@ -0,0 +1,18 @@
+<?php
+return "
+<div class='inlogkader'>
+		<img src='assets/Miauwer-Logo.png' alt='Logo miauwer' class='logo'>
+		<h1>Ai er liep iets fout!</h1>
+		<p>Je email of wachtwoord is fout, probeer opnieuw.</p>
+
+		<a class='teruginloggen' href='index.php?pagina=login'>Terug inloggen</a>
+
+
+
+		<div class='registreerkader'>
+			<p class='registerlink'>Nog geen miauwer-account?</p>
+			<p class='registerlink'> Maak er dan <a href='index.php?pagina=registratie'>hier</a> een aan!</p>
+		</div>
+</div>
+";
+?>
